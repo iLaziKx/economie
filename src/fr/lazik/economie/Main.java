80cc4318a1f6bdad56e758098a4b841b27f36233
Benoit Plaideau <benoit.plaideau@gmail.com>
2017-11-21 21:52:35 +0100

    push mardi soir

--- src/fr/lazik/economie/Main.java
+++ src/fr/lazik/economie/Main.java
@@ -1,11 +1,9 @@
 package fr.lazik.economie;
 
 import java.util.logging.Logger;
-
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
-import org.bukkit.command.Command;
-import org.bukkit.command.CommandSender;
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
@@ -16,13 +14,37 @@ import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class Main extends JavaPlugin implements Listener { // Nous rajoutons ceci !
+public class Main extends JavaPlugin implements Listener {
 
 	public Logger log = (Logger) java.util.logging.Logger.getLogger("Minecraft");
+	FileConfiguration config = getConfig();
 
 	public void onEnable() {
-		log.info("[GuideEpicube] Le plugin est correctement demarrer !");
-		Bukkit.getServer().getPluginManager().registerEvents(this, this); // Ici nous chargons les events !
+		config.addDefault("gain.ZOMBIE", 1);
+		config.addDefault("gain.SKELETON", 2);
+		config.addDefault("gain.SPIDER", 1);
+		config.addDefault("gain.CREEPER", 3);
+		config.addDefault("gain.SLIME", 1);
+		config.addDefault("gain.GHAST", 5);
+		config.addDefault("gain.PIG_ZOMBIE", 3);
+		config.addDefault("gain.ENDERMAN", 2);
+		config.addDefault("gain.BLAZE", 2);
+		config.addDefault("gain.WITCH", 5);
+		config.addDefault("gain.WITHER", 50);
+		config.addDefault("gain.WITHER_SKELETON", 10);
+
+		config.addDefault("prix.ironBlock", 20);
+		config.addDefault("prix.goldBlock", 50);
+		config.addDefault("prix.DiamondBlock", 150);
+		config.addDefault("prix.coalBlock", 5);
+		config.addDefault("prix.bread", 2);
+		config.addDefault("prix.cake", 20);
+		config.addDefault("prix.potatos", 16);
+		config.addDefault("prix.experience", 500);
+		config.options().copyDefaults(true);
+		saveConfig();
+		log.info("[E-conomie] Le plugin est correctement charge !");
+		Bukkit.getServer().getPluginManager().registerEvents(this, this);
 	}
 
 	public void onDisable() {
@@ -42,55 +64,21 @@ public class Main extends JavaPlugin implements Listener { // Nous rajoutons cec
 
 	@EventHandler
 	public void onEntityDeath(EntityDeathEvent event) {
+
 		Entity e = event.getEntity();
 		float gain = 0;
+		
 		if (e.getLastDamageCause() instanceof EntityDamageByEntityEvent) {
 			EntityDamageByEntityEvent nEvent = (EntityDamageByEntityEvent) e.getLastDamageCause();
+			Player player = (Player) nEvent.getDamager();
 			if (nEvent.getDamager() instanceof Player) {
-				Player p = (Player) nEvent.getDamager();
-				switch (e.getType()) {
-				case ZOMBIE:
-					gain = 1;
-					break;
-				case SKELETON :
-					gain = 2;
-					break;
-				case SPIDER :
-					gain = 1;
-					break;
-				case CREEPER:
-					gain = 3;
-					break;
-				case SLIME :
-					gain = 1;
-					break;
-				case GHAST :
-					gain = 5;
-					break;
-				case PIG_ZOMBIE:
-					gain = 3;
-					break;
-				case ENDERMAN:
-					gain = 2;
-					break;
-				case BLAZE :
-					gain = 2;
-					break;
-				case WITCH :
-					gain = 5;
-					break;
-				case WITHER :
-					gain = 50;
-					break;
-				case WITHER_SKELETON:
-					gain = 10;
-					break;
-				default:
-					gain = 0;
-					break;
+				player.sendMessage(e.getType().toString() + config.getStringList("gain.CREEPER"));
+				if (config.getStringList("gain").contains(e.getType().toString())) {
+					player.sendMessage("Vous avez tué un(e) " + e.getName() + " et gagné " + gain + "€");
 				}
-				p.sendMessage("Vous avez tué un(e) " + e.getType() + " et gagné " + gain + "€");
 			}
+
 		}
 	}
+
 }
